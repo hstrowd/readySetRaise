607a1f7efd7bc01c77e036abf88b07fdd76908b9
Harrison Strowd <hstrowd@clairvista.com>
2014-10-06 08:16:35 -0500

    Adds basic account creation/management support.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise gem.
+  config.action_mailer.default_url_options = { host: 'readySetRaise.com', port: 80 }
 end
