19ccf61b311626f32316a1a3edadfb42e63edba0
Harrison Strowd <hstrowd@clairvista.com>
2014-10-09 21:38:07 -0500

    Finishes off the initial version of the first section of the homepage. Styles the new organization page.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,32 @@
 class OrganizationsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @orgs = Organization.all
   end
 
-  # TODO: Require users to be logged in.
-  def create
+  def show
+    if params[:id]
+      @org = Organization.find_by_id(params[:id])
+    end
 
+    if !@org
+      flash[:alert] = 'Unable to find requested organization.'
+      redirect_to action: "index"
+    end
+  end
+
+  def new
+    @org = Organization.new
+  end
+
+  def create
+    @org = Organization.new(params[:organization])
+    if @org.save
+      redirect_to @org
+    else
+      render :new
+    end
   end
 
 end
