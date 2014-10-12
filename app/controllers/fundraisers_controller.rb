69464eeb62950066613dfc86093749d918071754
Harrison Strowd <hstrowd@clairvista.com>
2014-10-12 13:09:41 -0500

    Implements initial support for creating fundraisers.

--- app/controllers/fundraisers_controller.rb
+++ app/controllers/fundraisers_controller.rb
@@ -1,2 +1,35 @@
 class FundraisersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @fundraisers = Fundraiser.all
+  end
+
+  def show
+    if params[:id]
+      @fundraiser = Fundraiser.find_by_id(params[:id])
+    end
+
+    if !@fundraiser
+      flash[:alert] = 'Unable to find requested fundraiser.'
+      redirect_to action: "index"
+    end
+  end
+
+  def new
+    # TODO: Lookp the organizations for just this user.
+    @orgs = Organization.all
+    @fundraiser = Fundraiser.new
+  end
+
+  def create
+    @fundraiser = Fundraiser.new(params[:fundraiser])
+    if @fundraiser.save
+      # TODO: Redirect them to creating an event
+      redirect_to @fundraiser
+    else
+      render :new
+    end
+  end
+
 end
