038f64901d6070372e406410de01be143de59983
Harrison Strowd <hstrowd@clairvista.com>
2015-01-17 14:20:53 -0600

    Creates an initial team show page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :fundraiser
+  has_one :organization, :through => :fundraiser
   belongs_to :creator, :class_name => 'User'
 
   validates :title, :start_time, :end_time, :presence => true
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :event
+  has_one :fundraiser, :through => :event
+  has_one :organization, :through => :event
 
   validates :name, :presence => true
   validates_numericality_of :pledge_target
