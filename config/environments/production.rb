7082605c5e9515b87cfc4ef11f6c17bd6cd7286c
Harrison Strowd <hstrowd@clairvista.com>
2015-03-02 16:29:59 -0600

    Adds WOFF file to the set that are precompiled.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.png *.gif *.jpg *.jpeg *.ttf )
+  config.assets.precompile += %w( *.png *.gif *.jpg *.jpeg *.ttf *.woff )
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
